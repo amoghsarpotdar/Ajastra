40a1f613970cb327847b24da110a671e0b370ec1
Amogh <amogh.a.sarpotdar@gmail.com>
2020-11-07 14:03:49 +0000

    ExecPerfTest Fixed : Iteration 1.

--- Perft.cpp
+++ Perft.cpp
@@ -4,21 +4,27 @@
 void Perft::ExecPerfTest(int depth, S_BOARD* pos, Board board, BitboardProcessor bitboardproc)
 {
 	ASSERT(board.CheckBoard(pos, bitboardproc));
+	board.PrintBoard(pos);
 
-	if(depth == 0)
-	{
-		leafNodes++;
-		return;
-	}
-
+	printf("\nStarting test to Depth:%d\n", depth);
+	
+	leafNodes = 0;
 	S_MOVELIST list[1];
 	moveGenarator.GenerateAllMoves(pos,bitboardproc, list, board);
-
+	int move;
 	int MoveNum = 0;
 	for(MoveNum=0; MoveNum < list->count; ++MoveNum)
 	{
-		if (!makeMove.MakeMoveOnBoard(pos, list->moves[MoveNum].move, bitboardproc, board))
+		move = list->moves[MoveNum].move;
+		if (!makeMove.MakeMoveOnBoard(pos, move, bitboardproc, board)) {
 			continue;
+		}
+		long cumnodes = leafNodes;
+		ExecPerfTest(depth - 1, pos, board, bitboardproc);
+		makeMove.ReverseMoveOnBoard(pos, bitboardproc, board);
+		long oldnodes = leafNodes - cumnodes;
+		//printf("Move %d: %s : %ld\n", MoveNum + 1, PrMove(move), oldnodes);
 	}
+	printf("\nTest complete : %ld nodes visited\n", leafNodes);
 }
 
